8710719bded2cf9d2c714054aad35654f42bb7cc
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-11-03 23:41:15 +0530

    Setting up Cargo

---
+++ basic/target/debug/deps/basic-e48eae5ac543b4de.d
@@ -0,0 +1,5 @@
+C:\Users\PIYUSH\Desktop\RUST\basic\target\debug\deps\basic-e48eae5ac543b4de.d: src\main.rs
+
+C:\Users\PIYUSH\Desktop\RUST\basic\target\debug\deps\libbasic-e48eae5ac543b4de.rmeta: src\main.rs
+
+src\main.rs:
---
+++ basic/target/debug/deps/basic-f90986242cc05f3f.d
@@ -0,0 +1,5 @@
+C:\Users\PIYUSH\Desktop\RUST\basic\target\debug\deps\basic-f90986242cc05f3f.d: src\main.rs
+
+C:\Users\PIYUSH\Desktop\RUST\basic\target\debug\deps\libbasic-f90986242cc05f3f.rmeta: src\main.rs
+
+src\main.rs:
